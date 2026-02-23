b6b6205e993a6b553fa80ad63afd31df5ecb0185
itopup-mada <itopupmada@gmail.com>
2026-02-24 02:54:02 +0300

    Update login.js

--- api/login.js
+++ api/login.js
@@ -4,10 +4,17 @@ import jwt from "jsonwebtoken";
 
 const allowedOrigin = process.env.ALLOWED_ORIGIN;
 const SECRET = process.env.JWT_SECRET;
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_KEY)),
+    databaseURL: "https://tompon-daka-default-rtdb.firebaseio.com"
+  });
+}
+
 const db = admin.database();
 
 export default async function handler(req, res) {
-
   if (req.headers.origin !== allowedOrigin) {
     return res.status(403).json({ error: "Forbidden origin" });
   }
@@ -18,14 +25,12 @@ export default async function handler(req, res) {
   const { username, password } = req.body;
 
   const snap = await db.ref("users/" + username).get();
-
   if (!snap.exists())
     return res.status(400).json({ error: "User not found" });
 
   const user = snap.val();
 
   const valid = await bcrypt.compare(password, user.password);
-
   if (!valid)
     return res.status(400).json({ error: "Wrong password" });
 
